b133305e2dec8cc438f3b5211afb5e45650eb368
nshelton <nshelton@gmail.com>
2016-05-12 22:18:37 -0700

    Initial Commit

---
+++ shaderz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shaderz
+//
+//  Created by Nicholas Shelton on 5/12/16.
+//  Copyright © 2016 Nicholas Shelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shaderz/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  shaderz
+//
+
+//  Copyright (c) 2016 Nicholas Shelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
